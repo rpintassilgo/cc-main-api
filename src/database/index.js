c07b5c1dbdcd1817bb32b8690f6cd37bbfa9421c
Rodrigo Pintassilgo <rodrigofpintassilgo@gmail.com>
2024-06-28 00:53:03 +0100

    feat: update mongo env variables and the way it connects to db

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}`);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
