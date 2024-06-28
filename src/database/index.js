8364b19c13d1f7dfd59b4a25b228c865b2a1c24e
Rodrigo Pintassilgo <rodrigofpintassilgo@gmail.com>
2024-06-28 01:49:07 +0100

    feat: update mongo env

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}.${process.env.MONGO_URL}`);
+mongoose.connect(process.env.MONGO_URI);
 
 mongoose.Promise = global.Promise;
 
