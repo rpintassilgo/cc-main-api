724a3b84bdcb552479d9abaec98b65677b10bdc2
Rodrigo Pintassilgo <rodrigofpintassilgo@gmail.com>
2024-06-28 01:01:44 +0100

    feat: update mongo

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.set('strictQuery', false);
-mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}`);
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER_NAME}.${process.env.MONGO_URL}`);
+
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
